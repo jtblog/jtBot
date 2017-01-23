341e14fe29a955fbdec885267dba19ce38feb503
Obagbemisoye Temitope Joseph <jtob91@yahoo.com>
2017-01-23 15:59:44 +0100

    C

--- app/src/main/java/com/JT/MainActivity.java
+++ app/src/main/java/com/JT/MainActivity.java
@@ -2,13 +2,103 @@ package com.JT;
 
 import android.app.*;
 import android.os.*;
+import android.util.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.LinearLayout.*;
+import java.lang.reflect.*;
 
 public class MainActivity extends Activity 
 {
+	
+	public WebView mWebView;
+	public LinearLayout LL;
+	public ProgressBar PB;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		mWebView = new WebView(this);
+		
+		LL = (LinearLayout) findViewById(R.id.mainLinearLayout1);
+		PB = (ProgressBar) findViewById(R.id.mainProgressBar1);
+		
+		mWebView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+													 
+		// Enable Javascript
+		WebSettings webSettings = mWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
+		mWebView.setWebViewClient(new MyAppWebViewClient());
+		
+		LL.addView(mWebView, 1);
+		
+		mWebView.loadUrl("https://google.com");
+		
     }
+	
+	private static Object getFieldValueSafely(Field field, Object classInstance) throws IllegalArgumentException, IllegalAccessException {
+		boolean oldAccessibleValue = field.isAccessible();
+		field.setAccessible(true);
+		Object result = field.get(classInstance);
+		field.setAccessible(oldAccessibleValue);
+		return result;
+	}
+	
+	/**
+	 * Set Proxy for Android 4.1 and above.
+	 */
+	public boolean setProxy(WebView webview, String host, int port, String exclusionList) {
+
+		try {
+
+			Class wvcClass = Class.forName("android.webkit.WebViewClassic");
+			Class wvParams[] = new Class[1];
+			wvParams[0] = Class.forName("android.webkit.WebView");
+			Method fromWebView = wvcClass.getDeclaredMethod("fromWebView", wvParams);           
+			Object webViewClassic = fromWebView.invoke(null, webview);      
+
+			Class wv = Class.forName("android.webkit.WebViewClassic");
+			Field mWebViewCoreField = wv.getDeclaredField("mWebViewCore");
+			Object mWebViewCoreFieldIntance = getFieldValueSafely(mWebViewCoreField, webViewClassic);
+
+			Class wvc = Class.forName("android.webkit.WebViewCore");
+			Field mBrowserFrameField = wvc.getDeclaredField("mBrowserFrame");
+			Object mBrowserFrame = getFieldValueSafely(mBrowserFrameField, mWebViewCoreFieldIntance);
+
+			Class bf = Class.forName("android.webkit.BrowserFrame");
+			Field sJavaBridgeField = bf.getDeclaredField("sJavaBridge");
+			Object sJavaBridge = getFieldValueSafely(sJavaBridgeField, mBrowserFrame);
+
+			Class ppclass = Class.forName("android.net.ProxyProperties");
+			Class pparams[] = new Class[3];
+			pparams[0] = String.class;
+			pparams[1] = int.class;
+			pparams[2] = String.class;
+			Constructor ppcont = ppclass.getConstructor(pparams);
+
+			Class jwcjb = Class.forName("android.webkit.JWebCoreJavaBridge");
+			Class params[] = new Class[1];
+			params[0] = Class.forName("android.net.ProxyProperties");
+			Method updateProxyInstance = jwcjb.getDeclaredMethod("updateProxy", params);
+
+			updateProxyInstance.invoke(sJavaBridge, ppcont.newInstance(host, port, exclusionList));
+
+		} catch (Exception ex) {
+			Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_SHORT).show();
+			Log.e("","Setting proxy with >= 4.1 API failed with error: " + ex.getMessage());
+			return false;
+		}
+
+		Log.d("", "Setting proxy with >= 4.1 API successful!");
+		return true;
+	}
+	
+	public class MyAppWebViewClient extends WebViewClient {
+		
+    }
+	
 }
